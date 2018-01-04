a174fae04471b0df042765f3c78c97b24e88f1cb
Evan Green <evangreen@evans-macbook-pro.local>
2018-01-04 22:50:37 +0900

    Initial Commit

---
+++ Let's Do This/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Let's Do This
+//
+//  Created by Evan Green on 1/4/18.
+//  Copyright © 2018 Evan Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
